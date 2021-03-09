5a527116992a6ea0d8e33cfa074c93493dbdc3e2
2easy2do <enriquealexy@gmail.com>
2021-03-08 20:57:41 -0600

    changed color scheme, added copy button cleaned commented code no used

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,30 @@
   <div class="home"> 
     <h1 class="text-red-400">Put Left Side</h1>
     <div>
+      <h1 class="text-blue-500 font-black">Options</h1>
       <div> 
         <input v-model="caps" type="checkbox" id="caps" name="caps" value="caps">
-        <label for="caps"> Use Caps</label>
+        <label for="caps" class="text-white"> Use Caps</label>
       </div>
       <div>
         <input v-model="nums" type="checkbox" id="numbers" name="numbers" value="numbers">
-        <label for="numbers"> Use Number</label>
+        <label for="numbers" class="text-white"> Use Number</label>
       </div>
       <div>
         <input v-model="sym" type="checkbox" id="symbols" name="symbols" value="symbols">
-        <label for="symbols"> Use Symbols</label> 
+        <label for="symbols" class="text-white"> Use Symbols</label> 
       </div>
-    </div> 
-    <h1>choose to ahve a text box for user to inoput length instead of the slider</h1>
-    <input v-model="length" type="text" id="length" name="length"> 
+    <input v-model="length" type="text" id="length" name="length">  
+    </div>  
     <h1 class="text-red-400">Put Right Side</h1>
-    <h1>{{password}}</h1>
-    <button v-on:click="createpasswd" class="bg-red-400 rounded-md w-16 h-10 hover:bg-green-400 focus:bg-green-400 focus:ring-2 focus:outline-none focus:ring-red-400">Copy</button> 
-    <h1>button to save password to file in pc, it will also copy the password to clipboard</h1>
+    <div>
+      <h1 class="text-blue-500 font-black">Result</h1>
+      <h1 class="text-green-400">{{password}}</h1>
+      <div class="flex space-x-4 items-centerv justify-center">
+        <button v-on:click="createpasswd" class="text-black bg-red-400 rounded-md w-20 h-12 hover:bg-green-400 hover:text-white focus:bg-green-400 focus:ring-2 focus:outline-none focus:ring-red-400">Create Password</button> 
+        <button v-on:click="copy" class="text-black bg-red-400 rounded-md w-12 h-10 hover:bg-green-400 hover:text-white focus:bg-green-400 focus:ring-2 focus:outline-none focus:ring-red-400">Copy</button>  
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,7 +40,7 @@ export default {
   data() {
     return {
       password: "Click the button to make a new password",
-      length: 8,
+      length: 12,
       caps: false,
       nums: false,
       sym: false
@@ -53,17 +58,12 @@ export default {
     for(let i = 1; i <=this.length; i++){
       let char = Math.round(Math.random()
                             * letters.length + 1);
-     // Testing a new way of working with teh checkboxes
-     //if (this.caps == true){
-      //  if (Math.round(Math.random()) > 0){ 
-      //   this.password += letters.charAt(char).toUpperCase();
-      //  } else {
-      //    this.password += letters.charAt(char);
-      //  }
-      //}
       this.password += letters.charAt(char)
     } 
-    }
+    },
+  copy() {
+    navigator.clipboard.writeText(this.password)  
+  }
   }
 }
 
